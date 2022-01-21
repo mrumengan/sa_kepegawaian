908ddd816961ec3f802378d314f041f4be89ffdd
Legion\Mason Rumengan <masonrumengan@yahoo.com>
2022-01-21 13:09:05 +0700

    pembaruan pencarian presensi

--- common/models/PresensiSearch.php
+++ common/models/PresensiSearch.php
@@ -72,6 +72,10 @@ class PresensiSearch extends Presensi
             'longitude' => $this->longitude,
         ]);
 
+        if ($this->created_at_start == $this->created_at_end) {
+            $this->created_at_start = $this->created_at_start . ' 00:00:00';
+            $this->created_at_end = $this->created_at_end . ' 23:59:59';
+        }
         $query->andFilterWhere(['between', 'created_at', $this->created_at_start, $this->created_at_end]);
 
         $query->andFilterWhere(['like', 'address', $this->address]);
